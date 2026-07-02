8e36e5c862b567c496c4774e6f0907564dbbde11
david-r-heimann-usps <david.heimann@usps.gov>
2026-07-02 12:22:37 -0700

    Add Spring MVC controller and JSP view to webapp

---
+++ progreg-sec-webapp/src/main/java/com/progreg/webapp/controller/HelloController.java
@@ -0,0 +1,17 @@
+package com.progreg.webapp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/hello")
+public class HelloController {
+
+    @GetMapping
+    public String hello(Model model) {
+        model.addAttribute("message", "Hello from Spring MVC + JSP");
+        return "hello";
+    }
+}
---
+++ progreg-sec-webapp/src/main/webapp/WEB-INF/jsp/hello.jsp
@@ -0,0 +1,13 @@
+<%@ page contentType="text/html; charset=UTF-8" pageEncoding="UTF-8" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8"/>
+    <title>Hello</title>
+</head>
+<body>
+<h1>${message}</h1>
+<p>This JSP lives in WEB-INF/jsp and is rendered by a Spring MVC controller.</p>
+</body>
+</html>
