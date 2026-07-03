57897b7406fe000da48ac08640f0fd740948a490
Heimann, David - San Mateo, CA - Contractor <David.R.Heimann@usps.gov>
2026-07-02 21:50:24 -0400

    Update build-ear.bat

--- build-ear.bat
+++ build-ear.bat
@@ -1,53 +1,8 @@
-@echo off
-setlocal
-
 set "PROFILE=%~1"
 if "%PROFILE%"=="" set "PROFILE=websphere"
-set "DEFAULT_PROJECT_DIR=C:\Users\tkwtd0\pr"
-if "%PROJECT_DIR%"=="" set "PROJECT_DIR=%DEFAULT_PROJECT_DIR%"
-if not "%~2"=="" set "PROJECT_DIR=%~2"
-
-if defined JAVA_HOME_8 (
-    set "JAVA_HOME=%JAVA_HOME_8%"
-) else if defined JAVA8_HOME (
-    set "JAVA_HOME=%JAVA8_HOME%"
-)
-
-if not exist "%PROJECT_DIR%\pom.xml" (
-    echo ERROR: pom.xml not found in "%PROJECT_DIR%".
-    echo Update PROJECT_DIR in this script if your checkout path is different.
-    exit /b 1
-)
-
-where mvn >nul 2>&1
-if errorlevel 1 (
-    echo ERROR: Maven is not on PATH.
-    exit /b 1
-)
-
-echo Building EAR in "%PROJECT_DIR%" with profile "%PROFILE%"...
-if defined JAVA_HOME (
-    echo Using JAVA_HOME=%JAVA_HOME%
-) else (
-    echo JAVA_HOME not set by script. Using java from PATH.
-)
-
-pushd "%PROJECT_DIR%" || exit /b 1
-call mvn -version
-call mvn clean package -P%PROFILE%
-set "BUILD_RC=%ERRORLEVEL%"
-popd
-
-if not "%BUILD_RC%"=="0" (
-    echo Build failed with exit code %BUILD_RC%.
-    exit /b %BUILD_RC%
-)
-
-echo Build complete.
-set "EAR_PATH=%PROJECT_DIR%\progreg-sec-ear\target\progreg-sec.ear"
-if exist "%EAR_PATH%" (
-    echo EAR: %EAR_PATH%
-) else (
-    echo Build succeeded, but EAR file was not found at "%EAR_PATH%".
-)
-exit /b 0
+SET PRMAIN=c:\Users\tkwtd0\pr
+set PATH=%PRMAIN%\apache-maven-3.9.9\bin;%PRMAIN%\jdk-17\bin;%PRMAIN%\nodejs;%PATH%
+set JAVA_HOME=%PRMAIN%\jdk1.8.0
+set npm_config_cachex=%PRMAIN%\npmcache
+set npm_config_proxy=http://56.0.83.24:8080/
+mvn clean install -P%PROFILE%
